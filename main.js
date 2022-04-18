ec76d6a12fa9c70f5710378ac03b695914d802aa
abhinavmatchbox <ashokdecruz@gmail.com>
2022-04-18 18:35:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Lounge Bot is online');
+});
+
+
+
+client.login('OTY1NTg3Mjc3Nzc2NTYwMTUw.Yl1XOg.QD-8BchXBRwcVNgnuzGTlwjeoqc')
